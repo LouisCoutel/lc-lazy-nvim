e0fb05d9c0725e92732e2d9d61b85f5425a99381
LouisCoutel <louiscoutel75@gmail.com>
2024-04-20 17:47:16 +0200

    add new remaps for quitting, saving etc.

--- lua/lc/remap.lua
+++ lua/lc/remap.lua
@@ -2,3 +2,7 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>")
+vim.keymap.set({ "i", "x", "n", "s" }, "<C-s><C-q>", "<cmd>wq<cr><esc>")
+vim.keymap.set({ "i", "x", "n", "s" }, "<C-q>", "<cmd>q<cr><esc>")
+vim.keymap.set({"n","i"},"<C-q>!", "<cmd>q!<cr><esc>")
+vim.keymap.set("i","<C-c>","<esc>")
