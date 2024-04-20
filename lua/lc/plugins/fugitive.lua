54c0ddbebd9c67f11dd0bf99c1e18319fbcc2cb2
LouisCoutel <louiscoutel75@gmail.com>
2024-04-20 17:50:06 +0200

    configure fugitive with custom keymaps

---
+++ lua/lc/plugins/fugitive.lua
@@ -0,0 +1,31 @@
+return {"tpope/vim-fugitive",
+	dependencies={
+		"j-hui/fidget.nvim",
+		"rcarriga/nvim-notify"},
+	config = function()
+		vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+		vim.keymap.set("n", "<leader>gap", "<cmd>Git add --patch<cr>")
+		vim.keymap.set("n", "<leader>glo", "<cmd>Git log --oneline<cr>")
+		vim.keymap.set("n", "<leader>ggr", "<cmd>Git log --oneline --graph<cr")
+		vim.keymap.set("n", "<leader>gP", function ()
+			if vim.fn.confirm("Pull and rebase ?", "&yes\n&No") == 1 then
+				vim.cmd.Git({"pull","--rebase"})
+			end
+		end)
+
+		vim.keymap.set("n", "<leader>gPm", function ()
+			if vim.fn.confirm("Pull and merge ?", "&yes\n&No") == 1 then
+				vim.cmd("Git pull --rebase=false")
+			end
+		end)
+
+		vim.keymap.set("n","<leader>gc", "<cmd>Git commit<cr>")
+
+		vim.keymap.set("n", "<leader>gp", function ()
+			if vim.fn.confirm("Push ?", "&yes\n&No") == 1 then
+				vim.cmd("Git push")
+			end
+		end)
+
+	end,
+}
